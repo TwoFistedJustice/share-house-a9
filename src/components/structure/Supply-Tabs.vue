6d1f39f6e1703fffd40d5bc7fa7c0dd32983e434
Russ Bain <phaltak-2@yahoo.com>
2018-04-24 14:48:35 -0700

    removed data loop from tabs swiper to prvent bug

--- src/components/structure/Supply-Tabs.vue
+++ src/components/structure/Supply-Tabs.vue
@@ -7,27 +7,25 @@
       <f7-link tab-link="#tabSupply1" text="Shared"></f7-link>
       <f7-link tab-link="#tabSupply2" text="Shopping"></f7-link>
       <f7-link tab-link="#tabSupply3" text="Add/Delete"></f7-link>
-      <f7-link tab-link="#tabSupply4" text="Add Delete"></f7-link>
+      <f7-link tab-link="#tabSupply4" text="Blank"></f7-link>
     </f7-toolbar>
 <!-- Last tab won't work right - try making them routable!-->
-    <f7-tabs swipeable data-loop="true">
+    <!--<f7-tabs swipeable data-loop="true">-->
+    <f7-tabs swipeable data-loop="false">
       <f7-tab id="tabSupply1" tab-active>
-        <!--<supplies-modify></supplies-modify>-->
         <inventory-list></inventory-list>
       </f7-tab>
       <f7-tab id="tabSupply2" tab>
-        <!--<supplies-modify></supplies-modify>-->
-        <!--<shopping-list></shopping-list>-->
-        <inventory-list></inventory-list>
+        <shopping-list></shopping-list>
       </f7-tab>
       <f7-tab id="tabSupply3">
-        <!--<supplies-modify></supplies-modify>-->
-        <inventory-list></inventory-list>
+        <supplies-modify></supplies-modify>
       </f7-tab>
       <f7-tab id="tabSupply4">
-        <inventory-list></inventory-list>
-        <!--<supplies-modify></supplies-modify>-->
+        <p><-- Go back the other way</p>
+
       </f7-tab>
+
     </f7-tabs>
 
   </f7-page>
