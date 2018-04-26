0d0b1e61a354d6ddf3f07f7843dd0c2f563ab2d0
Russ Bain <phaltak-1@yahoo.com>
2018-04-26 16:24:22 -0700

    testing git

--- src/components/structure/Supply-Tabs.vue
+++ src/components/structure/Supply-Tabs.vue
@@ -22,7 +22,7 @@
         <supplies-modify></supplies-modify>
       </f7-tab>
       <f7-tab id="tabSupply4">
-        <p><-- Go back the other way remove this</p>
+        <p><-- Go back the other way remove this now</p>
 
       </f7-tab>
 
